272b347cd719b9dc048f5de75c447e418611b240
108360208 <stevenfreecom1@gmail.com>
2023-04-20 00:18:39 +0800

    3_update1

--- solution/121. Best Time to Buy and Sell Stock.cpp
+++ solution/121. Best Time to Buy and Sell Stock.cpp
@@ -11,3 +11,22 @@ public:
         return ans;
     }
 };
+//This method uses two pointers, l representing the buy position and r representing the sell position. 
+//If the difference between the values of pointer r and l is greater than zero, 
+//then update the answer by choosing the maximum value between prices[r]-prices[l] and the current answer. 
+//If not, move the l pointer to the position of r and increment r by 1. 
+//The concept of this method is quite special, and I couldn't come up with it when I tried to solve this problem.
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l=0;
+        int r=1;
+        int ans=0;
+        while(r<prices.size()){
+            if(prices[r]-prices[l]>0)ans=max(ans,prices[r]-prices[l]);
+            else l=r;
+            ++r;
+        }
+        return ans;
+    }
+};
